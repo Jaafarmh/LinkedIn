e52a403ecd601e1ce47dd750555ad95756d312e8
jaafar mohammad <jafarmh777@gamil.com>
2023-06-10 17:52:44 +0200

    final update

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,19 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import userReducer from '../features/userSlice';
+import  storage from 'redux-persist/lib/storage'
+import {persistReducer} from 'redux-persist';
+
+const persistConfig = {
+  key : 'root',
+  version : 1,
+  storage
+}
+
+const reducer =combineReducers({
+  user: userReducer,
+})
+const persistedReducer = persistReducer(persistConfig, reducer)
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
+  reducer: persistedReducer
 });
--- src/header.js
+++ src/header.js
@@ -10,8 +10,10 @@ import './header.css';
 import { logout } from './features/userSlice';
 import { signOut } from 'firebase/auth';
 import { auth } from './firebase';
-
+import { useState } from 'react';
+import MenuIcon from '@mui/icons-material/Menu';
 const Header = () => { 
+    const [headerIsOpen , setIsHeaderOpen] =useState(false)
     const dispatch = useDispatch();
     
     const logOutOfApp =()=>{
@@ -19,7 +21,11 @@ const Header = () => {
         signOut(auth);
         console.log('user logged out')
     }
-    
+    const openHeaderRight = ()=> {
+        setIsHeaderOpen(!headerIsOpen)
+    }
+   
+      
     return (
         <div className='header'>
         
@@ -33,6 +39,7 @@ const Header = () => {
 
             </div>
 
+        
             <div className="header__right">
                 <HeaderOption Icon={AddHomeIcon} title='Home' />
                 <HeaderOption Icon={SupervisorAccountIcon} title='My Network' />
@@ -43,9 +50,23 @@ const Header = () => {
                  title='Sign out'
                  onClick = {logOutOfApp}
                  myAvatar={true}  />
-            
             </div>
-            
+        
+          <MenuIcon className='hamburger' onClick={openHeaderRight}>Open</MenuIcon>
+
+        {headerIsOpen &&
+              <div className="header__mobile">
+                <HeaderOption Icon={AddHomeIcon} className='one' title='one' />
+                <HeaderOption Icon={SupervisorAccountIcon} title='two' />
+                <HeaderOption Icon={BusinessCenterIcon} title='Jobs' />
+                <HeaderOption Icon={ChatIcon} title='Messaging' />
+                <HeaderOption Icon={NotificationsIcon} title='Notifications' />
+                <HeaderOption 
+                 title='Sign out'
+                 onClick = {logOutOfApp}
+                 myAvatar={true}  />
+            </div>
+        }    
         </div>
     );
 }
